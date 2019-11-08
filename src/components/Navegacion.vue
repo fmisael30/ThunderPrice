939333cb87a02204595df6ea95f9e65e7260aecb
Richgomez100 <dassboymc@gmail.com>
2019-11-08 19:33:38 -0400

    mejorando navegacion

--- src/components/Navegacion.vue
+++ src/components/Navegacion.vue
@@ -5,7 +5,7 @@
 	<header class="header_area shadow">
     <div class="main_menu">
       <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container">
+        <div class="container"  style="color: #222 !important;">
           <router-link class="navbar-brand logo_h" to="/inicio"><img src="../assets/img/logo.png" width="200"></router-link>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,47 +15,23 @@
           </button>
           <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
             <ul class="nav navbar-nav menu_nav ml-auto mr-auto">
-              <li class="nav-item active"><router-link class="nav-link" to="/inicio">Home</router-link></li>
-              <li class="nav-item submenu dropdown">
-                <router-link to="/#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                  aria-expanded="false">Shop</router-link>
-                <ul class="dropdown-menu">
-                  <li class="nav-item"><router-link class="nav-link" to="/category">Shop Category</router-link></li>
-                  <li class="nav-item"><router-link class="nav-link" to="/single-product">Product Details</router-link></li>
-                  <li class="nav-item"><router-link class="nav-link" to="/checkout">Product Checkout</router-link></li>
-                  <li class="nav-item"><router-link class="nav-link" to="/confirmation">Confirmation</router-link></li>
-                  <li class="nav-item"><router-link class="nav-link" to="/cart">Shopping Cart</router-link></li>
-                </ul>
-							</li>
-              <li class="nav-item submenu dropdown">
-                <router-link to="/#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                  aria-expanded="false">Blog</router-link>
-                <ul class="dropdown-menu">
-                  <li class="nav-item"><router-link class="nav-link" to="/blog">Blog</router-link></li>
-                  <li class="nav-item"><router-link class="nav-link" to="/single-blog">Blog Details</router-link></li>
-                </ul>
-							</li>
-							<li class="nav-item submenu dropdown">
-                <router-link to="/#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                  aria-expanded="false">Pages</router-link>
-                <ul class="dropdown-menu">
-                  <li class="nav-item"><router-link class="nav-link" to="/login">Login</router-link></li>
-                  <li class="nav-item"><router-link class="nav-link" to="/register">Register</router-link></li>
-                  <li class="nav-item"><router-link class="nav-link" to="/recovery">Recovery</router-link></li>
-                  <li class="nav-item"><router-link class="nav-link" to="/crud">CRUD</router-link></li>
-                  <li class="nav-item"><router-link class="nav-link" to="/productos">Productos</router-link></li>
-                </ul>
-              </li>
-              <li class="nav-item"><router-link class="nav-link" to="/contact">Contact</router-link></li>
+              <li class="nav-item active"><router-link class="nav-link" to="/inicio" style="color:#222 !important;">Inicio</router-link></li>
+              <li class="nav-item"><router-link class="nav-link" to="/productos" style="color:#222 !important;">Productos</router-link></li>
+              <li class="nav-item"><router-link class="nav-link" to="/crud" style="color:#222 !important;">CRUD</router-link></li>
             </ul>
 
-            <ul class="nav-shop">
-              <li class="nav-item"><button><i class="ti-search"></i></button></li>
-              <li class="nav-item"><button><i class="ti-shopping-cart"></i><span class="nav-shop__circle">3</span></button> </li>
-              <li class="nav-item submenu dropdown">
-                <button data-toggle="dropdown" class="button button-header dropdown-toggle" v-if="usuario">{{usuario}}</button>
-                <div class="dropdown-menu">
-                  <button style="padding:0 12px; margin:auto !important;text-align:center;width:100%;" @click="Desloguearse">Cerrar sesion</button>
+            <ul class="d-flex" style="color:#222 !important;">
+              <li class="nav-item d-none"><button style="outline:none !important; border:none !important; background:none !important;"><i class="ti-search"></i></button></li>
+
+              <li class="nav-item mr-3" v-if="!usuario"><router-link to="/login" style="outline:none !important; border:none !important; background:none !important;">Login</router-link></li>
+              <li class="nav-item" v-if="!usuario"><router-link to="/register" style="outline:none !important; border:none !important; background:none !important;">Register</router-link></li>
+
+              <li class="nav-item" v-if="usuario"><router-link to="/carrito" style="color:#222 !important; outline:none !important; border:none !important; background:none !important;"><i class="ti-shopping-cart"></i></router-link> </li>
+              <li class="nav-item" v-if="usuario"></li>
+              <li class="nav-item submenu dropdown" v-if="usuario">
+                <button data-toggle="dropdown" class="dropdown-toggle" style="outline:none !important; border:none !important; background:none !important;"><i class="ti-user"></i></button>
+                <div class="dropdown-menu" style="left:auto !important; right:0 !important;">
+                  <button style="outline:none !important; border:none !important; background:none !important; padding:0 12px; margin:auto !important;text-align:center;width:100%;" @click="Desloguearse">Cerrar sesion</button>
                 </div>
               </li>
             </ul>
@@ -84,6 +60,16 @@ export default {
 
 
   created(){
+    let self = this;
+    if(firebase.auth().currentUser){
+        self.usuario = firebase.auth().currentUser.email
+    }
+    else{
+      this.usuario = null
+    }
+  },
+
+  updated(){
     let self = this;
     if(firebase.auth().currentUser){
         self.usuario = firebase.auth().currentUser.email
